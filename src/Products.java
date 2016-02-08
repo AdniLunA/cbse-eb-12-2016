a9e8c505236fc7aeeeb0f2701621756d47696d05
Linda <linda.latreider@web.de>
2016-02-08 14:51:06 +0100

    Orderlist

--- src/Products.java
+++ src/Products.java
@@ -1,18 +1,19 @@
-import java.util.Random;
-import java.math.*;
+
 
 public class Products {
     private double[] products = {12.34, 34.56, 1.23, 50.00, 1.00, 1.50, 2.99, 25.12, 6.50, 8.15, 10.20, 21.12};
     private double[] order;
-    private int numberOfProducts;
 
 
-    public void generateOrder(int numberOfProducts) {
+    public double[] generateOrder(){
 
-        for( int i = 0; i<numberOfProducts; i++)
-        {
 
-        }
+    for (int i = 0; i <= 5; i++) {
+        int randomNumber = (int)Math.round(Math.random()*11);
+        order[i] = products[randomNumber];
+
+
     }
-
+    return order;
+    }
 }

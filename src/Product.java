53929bdcec2b758100c19700c294103fe009e286
Linda <linda.latreider@web.de>
2016-02-08 14:33:22 +0100

    komischer Stand

--- src/Product.java
+++ src/Product.java
@@ -10,6 +10,24 @@ public class Product {
         this.price = (double) (Math.random()*100);
     }
 
+    public Product (String name, double price){
+        this.name = name;
+        this.price = price;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
---
+++ src/Products.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+import java.math.*;
+
+public class Products {
+    private double[] products = {12.34, 34.56, 1.23, 50.00, 1.00, 1.50, 2.99, 25.12, 6.50, 8.15, 10.20, 21.12};
+    private double[] order;
+    private int numberOfProducts;
+
+
+    public void generateOrder(int numberOfProducts) {
+
+        for( int i = 0; i<numberOfProducts; i++)
+        {
+            
+        }
+    }
+
+}
